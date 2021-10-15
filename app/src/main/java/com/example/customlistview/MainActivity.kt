cc709d88644f490a598bd39f18accb2a9420566a
Abhishek <absk.sharma7@gmail.com>
2021-10-15 23:41:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/customlistview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.customlistview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.customlistview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
